e70f6be00715978f0bc1c43c3a5a98e05f05fa5d
Alexandre Ferreira <aferreira.pro@gmail.com>
2020-02-12 15:41:40 +0100

    all green

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+puts 'Creating 10 fake restaurants...'
+10.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Movie.quote,
+    address: Faker::Address.full_address,
+    category:  CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+  restaurant.save!
+end
+puts 'Finished!'
