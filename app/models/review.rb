665449bc2067f1b5a6857541f9082257eaafcd1b
Alexandre Ferreira <aferreira.pro@gmail.com>
2020-02-12 12:04:55 +0100

    test 1

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  RATINGS = [0, 1, 2, 3, 4, 5]
+  validates :content, presence: true
+  validates :rating, inclusion: { in: RATINGS }, presence: true, numericality: { only_integer: true }
+end
